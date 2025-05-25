241d27fd0ec9f9faee97ac980b61fc968dfe8268
Irina Mihaela <irinamihaela.calota@ulbsibiu.ro>
2025-05-25 04:13:07 +0300

    added changes to price history feature

--- price-comparator/src/main/java/com/accesa/price_comparator/controller/ProductController.java
+++ price-comparator/src/main/java/com/accesa/price_comparator/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.accesa.price_comparator.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accesa.price_comparator.model.Product;
+import com.accesa.price_comparator.service.ProductHistoryService;
 import com.accesa.price_comparator.service.ProductService;
 
 @RestController
@@ -19,9 +22,12 @@ import com.accesa.price_comparator.service.ProductService;
 public class ProductController {
 
     private final ProductService productService;
+    private final ProductHistoryService productHistoryService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, ProductHistoryService priceHistoryService) {
         this.productService = productService;
+        this.productHistoryService = priceHistoryService;
+
     }
 
     @GetMapping
@@ -48,4 +54,14 @@ public class ProductController {
     public void delete(@PathVariable Long id) {
         this.productService.delete(id);
     }
+
+    @GetMapping("/price-history/trend")
+    public Map<String, Map<String, Map<String, Double>>> getTrend(
+            @RequestParam(required = false) Long productId,
+            @RequestParam(required = false) Long supplierId,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String brand) {
+        return productHistoryService.getProductsHistoryFiltered(productId, supplierId, category, brand);
+    }
+
 }
